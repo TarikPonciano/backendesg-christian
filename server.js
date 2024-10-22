bd4dc3daf97b3fada07b2312ae9c00432caf2a46
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2024-10-22 00:12:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,32 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // URL do banco de dados
   ssl: {
     rejectUnauthorized: true, // Verificar se o certificado é confiável
-    ca: fs.readFileSync('ca.pem').toString() // Caminho para o certificado
+    ca: `-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUVAPX5k9ubXpZn2ITBDkb2FRaPaAwDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvZWYxNjc2ZGMtZjMwMi00MmY4LThhNDktNmQ1Y2U2YzJl
+OTNhIFByb2plY3QgQ0EwHhcNMjQwOTI1MTIyMjA5WhcNMzQwOTIzMTIyMjA5WjA6
+MTgwNgYDVQQDDC9lZjE2NzZkYy1mMzAyLTQyZjgtOGE0OS02ZDVjZTZjMmU5M2Eg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAKq78bu3
+ANMWgF7aSJxGf52xcTUZQ/C29kG4AWnzal51+mca4cL7r4dldWtryRywJdDFaYbo
+CC88mNyWLv2th9dBHWeWm9dqQTyoQKnRe6gf9/xq3bX6jW+sJOMSVHL0QiOzk9Dp
+yX51aPJaaHYr26AvoaFEy9eYbBwpgC3Pc0uuFI5Lc208h7SUFNNLqcAnhBOYoYcL
+L6fxVngD5LlDIrtpKthv7XoAvyRshGXdWpAdJFDU46UZBqiNQepd4OQPKFQH1Idd
+K+BlG3wazCn+d+P6j2WT0/gMEfrXXCeYzvY8TxO8M/V0aHZdPRugBIwwtyqjIdg2
+uJpb89pFsXPPRHn1WpMSWRAu3r31cisFpWvv4em12DUjQKGm4XVibgqWk4U1cGJf
+tzM/LVzfLKwg4460DFKiqXUlwdK/U6rywt/mn19A0pCiu1pxeuTHqHH6US78Jcuh
+9pk3jnXQlAyDP3zhDm94HjKjgkzblU2M7sXqpf/qzG1V4qOalw7iaQaxVwIDAQAB
+oz8wPTAdBgNVHQ4EFgQUorXgwruZL1vx4t0rLc/ftLeLwIEwDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBAFdCFF0JpsGmMjEe
+r1H5jlo0amVQQ0XQQQxd6XO+MZwuLj3rtUjyf3xRvcvqpn+gcW2wSROCyMjIk626
++BUIfRTXWaWdc6PTrBgMc9x5my9aXKEH2Y1cHnAy9tkhIhQVXTvRx7xP8XHUhHaZ
+GQuFW1jpPq/WbrcOZSqE4HdK0Nz313yp9mFRTXZjzg65sfdlbnN4+hPLGC4MqPO2
+fBUJzEd7LX5fFBvNVksyPxM2qCh3vH4lZlZFtNtlJ0WY1kfAWeAJ6s7PbbOL1i9a
+7eVqqM7bdN/sCajRAR8FnOtxtAhrU/STJ+0SCthPwXocJ0u+laGearPTs7F67Pu9
+cUy64mXR4KAgDYqdvijTc2Gmyrusobl2YAjZedu4B1Gn3Pylj+QcJNeg8fJHT2OK
+suc53S4O6Wgg6tq67wJ0NTvdD9z3ilnWlqeINTtJ1BlFgfbVcEnlpOhaXGv//+RH
+JCfFhJUo/cZYOg8HmJQ8WwCy27USf9/H0XWU2yYDtsTFD5FZ2A==
+-----END CERTIFICATE-----
+` // Caminho para o certificado
   }
 });
 
